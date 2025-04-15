2cc3a9f18f5cde0993018d05496d8f73aa3d9136
Amit Goel <amit@kineti.nl>
2025-04-16 01:21:34 +0200

    css

--- app/(dashboard)/personas/page.tsx
+++ app/(dashboard)/personas/page.tsx
@@ -35,8 +35,8 @@ export default function PersonasPage() {
               Create New Persona
             </Button>
           </DialogTrigger>
-          <DialogContent className="sm:max-w-[500px] max-h-[90vh] overflow-hidden flex flex-col">
-            <DialogHeader className="px-6 pt-6 pb-2">
+          <DialogContent className="sm:max-w-[500px] max-h-[90vh] overflow-hidden flex flex-col pb-0 pt-0">
+            <DialogHeader className="px-2 pt-4 pb-4">
               <DialogTitle>Create New Persona</DialogTitle>
               <DialogDescription>Add a new AI participant persona for your simulations</DialogDescription>
             </DialogHeader>
@@ -99,7 +99,7 @@ export default function PersonasPage() {
               </div>
 
             </div>
-            <DialogFooter className="px-6 py-4 border-t">
+            <DialogFooter className="px-0 py-2 pb-2 border-t">
               <Button type="submit" onClick={() => setOpen(false)}>
                 Save Persona
               </Button>

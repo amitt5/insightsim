c12f97bb2ced8843800e326a34f30d40ad60e04c
Amit Goel <amit@kineti.nl>
2025-05-27 16:44:36 +0200

    updated model

--- app/api/run-simulation/route.ts
+++ app/api/run-simulation/route.ts
@@ -13,10 +13,10 @@ export async function POST(req: Request) {
     console.log('prompt123', messages);
     // Call OpenAI
     const completion = await openai.chat.completions.create({
-      model: 'gpt-3.5-turbo',
+      model: 'gpt-4o-mini',
       messages,
-      temperature: 0.7,
-      max_tokens: 1000,
+      temperature: 0.9,
+      max_tokens: 3000,
     //   top_p: 1,
     //   frequency_penalty: 0,
     //   presence_penalty: 0,

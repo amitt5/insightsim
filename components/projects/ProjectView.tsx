b0044312d797978caff71f35d232236c08eae451
Amit Goel <amit@kineti.nl>
2025-11-13 02:45:39 -0800

    ask maira

--- components/projects/ProjectView.tsx
+++ components/projects/ProjectView.tsx
@@ -1465,7 +1465,7 @@ export default function ProjectView({ project, onUpdate }: ProjectViewProps) {
           <TooltipProvider>
             <Tooltip>
               <TooltipTrigger asChild>
-                <TabsTrigger value="ask-analysis" disabled={!briefText || !hasAnalysis}>Ask Analysis(beta)</TabsTrigger>
+                <TabsTrigger value="ask-analysis" disabled={!briefText || !hasAnalysis}>Ask Maira(beta)</TabsTrigger>
               </TooltipTrigger>
               {!briefText ? (
                 <TooltipContent>
